ad11b39c1635295ddd7811989b7306697ed8de41
sz2 <sugiura.k.nigo@gmail.com>
2023-01-22 13:29:42 +0900

    Remove ElectronNET codes.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -21,7 +20,7 @@ namespace ECUSimGUI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseElectron(args).UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
